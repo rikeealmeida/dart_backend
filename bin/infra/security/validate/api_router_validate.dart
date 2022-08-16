e3b7e10b5b28b1a3e88ceb39eaf0086b9e910743
rikeealmeida <henrique.almeida.ads@gmail.com>
2022-08-16 08:33:52 -0300

    jwt finished

---
+++ bin/infra/security/validate/api_router_validate.dart
@@ -0,0 +1,12 @@
+class ApiRouterValidate {
+  List<String> _routes = [];
+
+  ApiRouterValidate add(String route) {
+    _routes.add(route);
+    return this;
+  }
+
+  bool isPublic(String route) {
+    return _routes.contains(route);
+  }
+}
